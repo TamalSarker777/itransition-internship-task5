b4e1b4f42905ed16b205ba8a177f4143c8a7e69e
Tamal <155307336+TamalSarker777@users.noreply.github.com>
2024-09-28 00:30:27 +0600

    Add files via upload

---
+++ task5/src/App.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { FaShuffle } from "react-icons/fa6";
+import { Navbar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import { generateUserData, applyErrors } from "./components/dataGenerators";
+import Table from "react-bootstrap/Table";
+
+function App() {
+  const [seletedCountry, setSelectedCountry] = useState("usa");
+  const [sliderValue, setSliderValue] = useState(0);
+  const [seedValue, setSeedValue] = useState(42);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const newUsers = generateUserData(seletedCountry, seedValue, 1); // Initially page is 1
+    const usersWithErrors = applyErrors(newUsers, sliderValue);
+    setUsers(usersWithErrors);
+  }, [seletedCountry, seedValue, sliderValue]);
+
+  const getSelectedRegion = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
+  const handleSlider = (event) => {
+    const value = Number(event.target.value);
+    if (value > 1000 || value < 0) {
+      alert("The value should be from 0 to 1000");
+    } else {
+      setSliderValue(event.target.value);
+      const usersWithErrors = applyErrors(users, sliderValue);
+      setUsers(usersWithErrors);
+    }
+  };
+
+  const handleSeed = (event) => {
+    setSeedValue(event.target.value);
+  };
+
+  function randomSeed() {
+    setSeedValue(Math.floor(Math.random() * 10000) + 1);
+  }
+
+  // -------------------------------------------
+  const loadMore = useCallback(() => {
+    const newUsers = generateUserData(seletedCountry, seedValue, 2);
+    const usersWithErrors = applyErrors(newUsers, sliderValue);
+
+    setUsers((prevUsers) => [...prevUsers, ...usersWithErrors]);
+  }, [seletedCountry, seedValue, sliderValue]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop >=
+        document.documentElement.offsetHeight
+      ) {
+        loadMore();
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+  }, [loadMore]);
+
+  // ---------------------------------------------------------------
+
+  return (
+    <>
+      <Navbar expand="lg" className="bg-dark">
+        <Navbar.Brand href="#" className="text-white">
+          Task 5
+        </Navbar.Brand>
+      </Navbar>
+      <br />
+      <br />
+      <div class="container">
+        <div class="column">
+          <label for="cars">
+            <strong>Region: </strong>
+          </label>
+          <select
+            name="region"
+            id="region"
+            onChange={getSelectedRegion}
+            value={seletedCountry}
+          >
+            <option value="usa">USA</option>
+            <option value="poland">Poland</option>
+            <option value="georgia">Georgia</option>
+          </select>
+        </div>
+
+        <div class="column">
+          <label htmlFor="sldier">
+            <strong>Error: </strong>
+          </label>
+          <input
+            type="range"
+            name="slider"
+            id="slider"
+            min="0"
+            max="10"
+            value={sliderValue}
+            onChange={handleSlider}
+          />
+          <input
+            type="number"
+            id="errorInput"
+            value={sliderValue}
+            min="0"
+            max="1000"
+            onChange={handleSlider}
+          />
+        </div>
+        <div class="column">
+          <label for="seed">
+            <strong>Seed: </strong>
+          </label>
+          <input
+            type="number"
+            name="seed"
+            id="seed"
+            value={seedValue}
+            onChange={handleSeed}
+          />
+          <Button
+            type="button"
+            value={seedValue}
+            onClick={randomSeed}
+            variant="info"
+          >
+            <FaShuffle />
+          </Button>
+        </div>
+        <div class="column">
+          <Button type="button" variant="success">
+            Export
+          </Button>
+        </div>
+      </div>
+      <br />
+      {/* -------------------------------------------------------- */}
+
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Identifier</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user.id}>
+              <td>{index + 1}</td>
+              <td>{user.randomIdent}</td>
+              <td>{user.name}</td>
+              <td>{user.address}</td>
+              <td>{user.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+
+export default App;
---
+++ task5/src/components/dataGenerators.js
@@ -0,0 +1,118 @@
+//-----------------------------------------
+import seedrandom from "seedrandom";
+import { fakerEN_US, fakerPL, fakerKA_GE, fakerEN } from "@faker-js/faker";
+
+export const generateUserData = (region, seed, pageNumber) => {
+  let faker;
+  if (region === "usa") {
+    faker = fakerEN_US;
+  } else if (region === "poland") {
+    faker = fakerPL;
+  } else if (region === "georgia") {
+    faker = fakerKA_GE;
+  } else {
+    faker = fakerEN;
+  }
+
+  const randomseed = seedrandom(seed + pageNumber);
+  faker.seed(Math.floor(randomseed() * 10000) + 1);
+
+  const users = [];
+  let total_range;
+  if (pageNumber === 1) {
+    total_range = 20;
+  } else {
+    total_range = 10;
+  }
+  for (let i = 0; i < total_range; i++) {
+    users.push({
+      randomIdent: faker.string.uuid(),
+      name: faker.person.fullName(),
+      address: `${faker.location.city()}, ${faker.location.streetAddress()}, ${faker.location.zipCode()}`,
+      phone: faker.phone.number(),
+    });
+  }
+  return users;
+};
+
+const swapRandomCharacter = (str) => {
+  let charArray;
+  if (Number.isInteger(str)) {
+    str = str.toString();
+  }
+  charArray = str.split("");
+
+  const t1 = Math.floor(Math.random() * charArray.length);
+  const t2 = Math.floor(Math.random() * charArray.length);
+  let mini = Math.min(t1, t2);
+  let maxi = Math.max(t1, t2);
+
+  const temp = charArray[mini];
+  charArray[mini] = charArray[maxi];
+  charArray[maxi] = temp;
+
+  const strJoin = charArray.join("");
+
+  return strJoin;
+};
+
+const deleteRandomCharacter = (str) => {
+  if (Number.isInteger(str)) {
+    str = str.toString();
+  }
+
+  const randomValue = Math.floor(Math.random() * str.length);
+
+  let final_str = str.slice(0, randomValue) + str.slice(randomValue + 1);
+  return final_str;
+};
+
+const addRandomCharacter = (str) => {
+  if (Number.isInteger(str)) {
+    str = str.toString();
+  }
+
+  const randomindex = Math.floor(Math.random() * str.length);
+  const characters = "abcdefghijklmnopqrstuvwxyz";
+
+  const randomValue = Math.floor(Math.random() * characters.length);
+  const randomCharacter = characters.charAt(randomValue);
+
+  let final_str =
+    str.slice(0, randomindex) + randomCharacter + str.slice(randomindex);
+
+  return final_str;
+};
+
+export const applyErrors = (userData, totalErrors) => {
+  let modifiedData = [...userData];
+
+  for (let i = 0; i < totalErrors; i++) {
+    const randomUserIndex = Math.floor(Math.random() * modifiedData.length);
+    const randomUser = { ...modifiedData[randomUserIndex] };
+
+    const fields = ["randomIdent", "name", "address", "phone"];
+    const randomField = fields[Math.floor(Math.random() * 4)];
+
+    const errorType = Math.floor(Math.random() * 3);
+
+    const value = randomUser[randomField];
+    switch (errorType) {
+      case 0:
+        randomUser[randomField] = deleteRandomCharacter(value);
+        break;
+      case 1:
+        randomUser[randomField] = addRandomCharacter(value);
+        break;
+      case 2:
+        randomUser[randomField] = swapRandomCharacter(value);
+        break;
+      default:
+        alert("Sorry..! try again..");
+    }
+
+    modifiedData[randomUserIndex] = randomUser;
+  }
+
+  return modifiedData;
+};
